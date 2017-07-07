67425bdf03495956de7c25494b2621949993f214
Chris DeCarolis <cdecarolis289@gmail.com>
2017-07-06 21:45:24 -0400

    First commit:Maze conversion to greyscale and internal representation

---
+++ DetectMaze.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+'''
+Purpose: 
+Given RGB image, convert to grayscale, then read it in as a matrix of 1's and 0's
+'''
+
+def representation(filePath):
+	'''
+	takes an image of maze and converts it to array of 1's and 0's
+	'''
+	imageFile=Image.open(filePath)#open image
+	imageFile=imageFile.convert('1')#convert image to grayscale
+	pixels=list(imageFile.getdata())#iterable list of pixels in image
+	width,height=imageFile.size#get height and width of image(in pixels)
+	pixels=[pixels[i * width:(i + 1) * width] for i in range(height)]
+	print ([[1 if pixel==255 else 0 for pixel in row] for row in pixels])
+
+representation('test_image.png')
+
